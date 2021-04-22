6d0284ff815e45537e34316574c5a05b3714fdd8
Perryvon <Perry.j.vr@gmail.com>
2021-04-22 12:11:35 -0700

    first page loaded succesfully first handoff

--- app.py
+++ app.py
@@ -7,3 +7,26 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+RESPONSES = []
+
+@app.route('/')
+def show_title_page():
+    ''' render a page that shows the user the title of the survey, 
+       the instructions, and a button to start the survey. '''
+
+    
+    title = survey.title
+    instructions = survey.instructions
+
+
+    return render_template("survey_start.html",
+    survey_title = title, 
+    survey_instructions = instructions )
+
+
+
+# surveys = {
+#     "satisfaction": satisfaction_survey,
+#     "personality": personality_quiz,
+# }
